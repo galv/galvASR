8ef7e8ddb6f504ae8b68a962eec2e74e8baba5b4
Daniel Galvez <dt.galvez@gmail.com>
2017-12-04 09:28:08 -0800

    Get mini_librispeech recipe working up to
    Example way to run:
    
    ./run.py --raw-data-dir /export/a05/dgalvez/ --kaldi-data-dir data/ --exp-dir exp/ --stage 0
    
    Kaldi submodule points to a commit in my personal github repo of Kaldi
    until https://github.com/kaldi-asr/kaldi/pull/2061 gets
    merged.

--- galvASR/examples/mini_librispeech/run.py
+++ galvASR/examples/mini_librispeech/run.py
@@ -7,11 +7,12 @@ from __future__ import print_function
 import click
 import os
 import subprocess
-import sys
+import tempfile
 
 from galvASR.python import kaldi_environment
 
 kaldi_environment.setup_environment()
+
 
 @click.command()
 @click.option('--stage', default=0)
@@ -19,7 +20,10 @@ kaldi_environment.setup_environment()
 @click.option('--kaldi-data-dir', type=str)
 @click.option('--mini-librispeech-kaldi-dir',
               default=os.path.join(os.environ['KALDI_ROOT'], 'egs/mini_librispeech/s5/'))
-def run(stage, raw_data_dir, kaldi_data_dir, mini_librispeech_kaldi_dir):
+@click.option('--exp-dir', default='exp')
+@click.option('--oov-symbol', default="<UNK>")
+def run(stage, raw_data_dir, kaldi_data_dir, mini_librispeech_kaldi_dir,
+        exp_dir, oov_symbol):
     # If only this script could inherit --mini-librispeech-kaldi-dir
     # automatically.
     def call_local(script, args):
@@ -40,13 +44,46 @@ def run(stage, raw_data_dir, kaldi_data_dir, mini_librispeech_kaldi_dir):
                        [os.path.join(raw_data_dir, 'LibriSpeech', part),
                         os.path.join(kaldi_data_dir, part.replace('-', '_'))])
 
-    
+        call_local('prepare_dict.sh',
+                   ['--stage', '3', os.path.join(kaldi_data_dir, 'local', 'lm'),
+                    os.path.join(kaldi_data_dir, 'local', 'lm'),
+                    os.path.join(kaldi_data_dir, 'local', 'dict_nosp')])
+        subprocess.check_call(['utils/prepare_lang.sh',
+                               os.path.join(kaldi_data_dir, 'local', 'dict_nosp'),
+                               oov_symbol,
+                               os.path.join(kaldi_data_dir, 'local', 'lang_tmp_nosp'),
+                               os.path.join(kaldi_data_dir, 'lang_nosp')])
+        call_local('format_lms.sh',
+                   ['--src-dir', os.path.join(kaldi_data_dir, 'lang_nosp'),
+                    os.path.join(kaldi_data_dir, 'local', 'lm')])
+        subprocess.check_call(['utils/build_const_arpa_lm.sh',
+                               'data/local/lm/lm_tglarge.arpa.gz',
+                               'data/lang_nosp', 'data/lang_nosp_test_tglarge'])
 
-@click.command()
-@click.argument(u'--data-dir')
-def download(data_dir):
-    pass
+    if stage <= 3:
+        with tempfile.NamedTemporaryFile('w+', suffix='conf') as mfcc_config:
+            mfcc_config.write("""\
+--use-energy=false   # use average of log energy, not energy.
+--num-mel-bins=40    # similar to Google's setup.
+--num-ceps=40        # there is no dimensionality reduction.
+--low-freq=20        # low cutoff frequency for mel bins... this is high-bandwidth data, so
+                     # there might be some information at the low end.
+--high-freq=-400     # high cutoff frequently, relative to Nyquist of 8000 (=7600)""")
+            for part in ('dev_clean_2', 'train_clean_5'):
+                subprocess.check_call(['steps/make_mfcc.sh', '--cmd', os.environ['train_cmd'],
+                                       '--mfcc-config', mfcc_config.name,
+                                       os.path.join(kaldi_data_dir, part),
+                                       os.path.join(exp_dir, 'make_mfcc', part),
+                                       os.path.join(kaldi_data_dir, 'mfcc')])
+                subprocess.check_call(['steps/compute_cmvn_stats.sh',
+                                       os.path.join(kaldi_data_dir, part),
+                                       os.path.join(exp_dir, 'make_mfcc', part),
+                                       os.path.join(kaldi_data_dir, 'mfcc')])
+
+    if stage <= 4:
+        pass
 
 
 if __name__ == u'__main__':
-    run()
+    with kaldi_environment.kaldi_load_utils_and_steps():
+        run()
--- galvASR/python/kaldi_environment.py
+++ galvASR/python/kaldi_environment.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from contextlib import contextmanager
 import logging
 import os
 
@@ -18,7 +19,7 @@ def setup_environment():
                  'to submodule path: %s', KALDI_ROOT)
         os.environ['KALDI_ROOT'] = KALDI_ROOT
     if os.environ.get('OPENFST_ROOT') is None:
-        OPENFST_ROOT = os.path.join(GALVASR_ROOT, 'third_party', 'kaldi', 'kaldi')
+        OPENFST_ROOT = os.path.join(GALVASR_ROOT, 'third_party', 'openfst')
         log.info('OPENFST_ROOT environment variable not already set. Defaulting '
                  'to symlink path (to Kaldi\'s install): %s', OPENFST_ROOT)
         os.environ['OPENFST_ROOT'] = OPENFST_ROOT
@@ -32,34 +33,73 @@ def setup_environment():
     # This is a modified copy-pasta of
     # $KALDI_ROOT/tools/config/common_path.sh
     os.environ['PATH'] = """\
-${KALDI_ROOT}/src/bin:\
-${KALDI_ROOT}/src/chainbin:\
-${KALDI_ROOT}/src/featbin:\
-${KALDI_ROOT}/src/fgmmbin:\
-${KALDI_ROOT}/src/fstbin:\
-${KALDI_ROOT}/src/gmmbin:\
-${KALDI_ROOT}/src/ivectorbin:\
-${KALDI_ROOT}/src/kwsbin:\
-${KALDI_ROOT}/src/latbin:\
-${KALDI_ROOT}/src/lmbin:\
-${KALDI_ROOT}/src/nnet2bin:\
-${KALDI_ROOT}/src/nnet3bin:\
-${KALDI_ROOT}/src/nnetbin:\
-${KALDI_ROOT}/src/online2bin:\
-${KALDI_ROOT}/src/onlinebin:\
-${KALDI_ROOT}/src/sgmm2bin:\
-${KALDI_ROOT}/src/sgmmbin:\
-${KALDI_ROOT}/src/tfrnnlmbin:\
-${OPENFST_ROOT}/bin:\
-${PATH}""".format(**os.environ)
+{KALDI_ROOT}/src/bin:\
+{KALDI_ROOT}/src/chainbin:\
+{KALDI_ROOT}/src/featbin:\
+{KALDI_ROOT}/src/fgmmbin:\
+{KALDI_ROOT}/src/fstbin:\
+{KALDI_ROOT}/src/gmmbin:\
+{KALDI_ROOT}/src/ivectorbin:\
+{KALDI_ROOT}/src/kwsbin:\
+{KALDI_ROOT}/src/latbin:\
+{KALDI_ROOT}/src/lmbin:\
+{KALDI_ROOT}/src/nnet2bin:\
+{KALDI_ROOT}/src/nnet3bin:\
+{KALDI_ROOT}/src/nnetbin:\
+{KALDI_ROOT}/src/online2bin:\
+{KALDI_ROOT}/src/onlinebin:\
+{KALDI_ROOT}/src/sgmm2bin:\
+{KALDI_ROOT}/src/sgmmbin:\
+{KALDI_ROOT}/src/tfrnnlmbin:\
+{OPENFST_ROOT}/bin:\
+{PATH}""".format(**os.environ)
 
-    # Get easy access to everything utils/ and steps/ (note these are
-    # symlinks in all the Kaldi recipes), but originally defined in WSJ.
-    os.environ['PATH'] = """\
-${KALDI_ROOT}/egs/wsj/s5/:\
-${PATH}
-""".format(**os.environ)
+    # Add utils/ to path directly for historical reasons.
+    # https://github.com/kaldi-asr/kaldi/issues/2058
+    # Prefer to make calls with the utils/ prefix, though.
+    os.environ['PATH'] = "{KALDI_ROOT}/egs/wsj/s5/utils/:{PATH}".format(**os.environ)
 
     # Search for "LC_ALL" in http://kaldi-asr.org/doc/data_prep.html
     # for a discussion of the importance of this environment variable.
     os.environ['LC_ALL'] = 'C'
+
+    # Normally the contents of cmd.sh. May want to make these
+    # configurable at some point.
+    os.environ['train_cmd'] = 'run.pl'
+    os.environ['decode_cmd'] = 'run.pl'
+
+
+@contextmanager
+def kaldi_load_utils_and_steps():
+    save_cwd = os.getcwd()
+    steps = os.path.join(save_cwd, 'steps')
+    utils = os.path.join(save_cwd, 'utils')
+    path_sh = os.path.join(save_cwd, 'path.sh')
+    cmd_sh = os.path.join(save_cwd, 'cmd.sh')
+    # While this should catch exceptions, and clean up the symlinks,
+    # there is a small chance that python will be killed (e.g., kill
+    # -9) without the exit portion of this function running, so don't
+    # error out if the symlink already exists.
+    if not os.path.islink(steps):
+        os.symlink('{KALDI_ROOT}/egs/wsj/s5/steps'.format(**os.environ), steps)
+    if not os.path.islink(utils):
+        os.symlink('{KALDI_ROOT}/egs/wsj/s5/utils'.format(**os.environ), utils)
+    with open(path_sh, 'w+'), open(cmd_sh, 'w+'):
+        # Some legacy kaldi scripts insist upon loading path.sh in the
+        # current workind directory, though this is wholly unnecessary
+        # since it's a precondition to calling those scripts that the
+        # environment is set up correctly. setup_environment()
+        # alreadys sets up the environment variables as path.sh would,
+        # so we just create an empty file. Same goes for cmd.sh
+        pass
+    try:
+        yield
+    finally:
+        if os.getcwd() != save_cwd:
+            log.warning('You changed your working directory. Kaldi\'s steps and '
+                        'utils symlinks and path.sh and cmd.sh will still be '
+                        ' cleaned up, but this is generally discouraged.')
+        os.remove(steps)
+        os.remove(utils)
+        os.remove(path_sh)
+        os.remove(cmd_sh)
